f4818ad52e4371626b0e0f4b0f8148ae4f41aa3d
Monica Björk <m.bjork95@gmail.com>
2018-07-21 01:40:51 +0200

    Menu is coming together.

--- JoppesDjurfamilj/Petowner.cs
+++ JoppesDjurfamilj/Petowner.cs
@@ -13,5 +13,61 @@ namespace JoppesDjurfamilj {
         private List<Ball> balls = new List<Ball>();
         private List<string> foods = new List<string>();
 
+
+
+        public void Menu() {
+            Console.WriteLine("=== Welcome to {0}'s Family of Pets ===", name);
+            Console.WriteLine("[L] List animals\n" +
+                              "[S] Storage\n" +
+                              "[A] About this program\n" +
+                              "[Q] Quit\n");
+            ConsoleKeyInfo userInputMainMenu = Console.ReadKey(true);
+            switch(userInputMainMenu.Key) {
+                case ConsoleKey.L: {
+                    // TODO: List animals, info, choose animal to interact with
+                    break;
+                }
+                case ConsoleKey.S: {
+                    // TODO: Undermenu to show availabe food and balls.
+                    Console.WriteLine("=== The Storage ===\n *some info*");
+                    Console.WriteLine("[F] Foods\n" +
+                                      "[B] Balls\n" +
+                                      "[R] Return\n");
+                    ConsoleKeyInfo userInputSubMenuStorage = Console.ReadKey(true);
+                    switch(userInputSubMenuStorage.Key) {
+                        case ConsoleKey.F: {
+                            // TODO: Show food
+                            break;
+                        }
+                        case ConsoleKey.B: {
+                            // TODO: Show balls
+                            break;
+                        }
+                        case ConsoleKey.R: {
+                            // TODO: Return
+                            break;
+                        }
+                        default: {
+                            // TODO: If the user don't make a menu choise...
+                            break;
+                        }
+                    }
+                    break;
+                }
+                case ConsoleKey.A: {
+                    // TODO: About this program and how to use it.
+                    break;
+                }
+                case ConsoleKey.Q: {
+                    // TODO: Quit the program
+                    break;
+                }
+                default: {
+                    // TODO: If the user don't make a menu choise...
+                    break;
+                }
+            }
+
+        }
     }
 }
--- JoppesDjurfamilj/Program.cs
+++ JoppesDjurfamilj/Program.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 namespace JoppesDjurfamilj {
     class Program {
         static void Main(string[] args) {
+            Petowner petowner = new Petowner();
+            petowner.Menu();
+            Console.WriteLine("The End");
+            Console.ReadKey();
         }
     }
 }
